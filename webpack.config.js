cf0efcd99a650ffc2cce59a84141b2cf96e94379
Efrain Hernandez <efrainhg2050@gmail.com>
2021-11-24 17:00:28 -0600

    css extra plugin configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 //const path = require('path');
 
 module.exports = {
@@ -27,7 +28,7 @@ module.exports = {
       },
       {
         test: /styles.css$/,
-
+        use: [ MiniCssExtractPlugin.loader, 'css-loader' ],
       }
   ]
   },
@@ -35,5 +36,11 @@ module.exports = {
     new HtmlWebpackPlugin({
         template: './src/index.html',
         filename: './index.html'
-  })],
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      ignoreOrder: false,
+    })
+  ],
 };
