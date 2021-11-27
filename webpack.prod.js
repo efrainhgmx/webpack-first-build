89dc35e157e51eb36312a2c79abc5ac2697b6a7a
Efrain Hernandez <efrainhg2050@gmail.com>
2021-11-27 16:38:50 -0600

    minimizer css for production

---
+++ webpack.prod.js
@@ -0,0 +1,66 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const CssMinimizer = require('css-minimizer-webpack-plugin');
+const Terser = require('terser-webpack-plugin');
+//const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    clean: true,
+    filename: 'main.[contenthash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {
+            sources: false,
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: /styles.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /styles.css$/,
+        use: [ MiniCssExtractPlugin.loader, 'css-loader' ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+      }
+  ]
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizer(),
+      new Terser(),
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: './index.html'
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].[fullhash].css',
+      ignoreOrder: false,
+    }),
+
+    new CopyPlugin({
+      patterns: [
+        { from: "src/assets", to: "assets" },
+      ],
+    })
+  ],
+};
