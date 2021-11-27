4b301dda7476c4627ff54fc3a522f2e9e4094bc1
Efrain Hernandez <efrainhg2050@gmail.com>
2021-11-27 16:52:52 -0600

    babel config

--- webpack.prod.js
+++ webpack.prod.js
@@ -34,7 +34,17 @@ module.exports = {
       {
         test: /\.(png|jpe?g|gif)$/i,
         loader: 'file-loader',
-      }
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
   ]
   },
 
